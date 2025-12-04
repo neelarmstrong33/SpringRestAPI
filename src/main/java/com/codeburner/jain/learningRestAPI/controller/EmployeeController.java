0b4827894c5e84e2949ff24fd0ef8ca1a2875c26
Neel <neelarmstrong33@gmail.com>
2025-12-04 16:18:19 +0530

    Part1 commit

--- src/main/java/com/codeburner/jain/learningRestAPI/controller/EmployeeController.java
+++ src/main/java/com/codeburner/jain/learningRestAPI/controller/EmployeeController.java
@@ -1,7 +1,32 @@
 package com.codeburner.jain.learningRestAPI.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codeburner.jain.learningRestAPI.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
+
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
+        return new EmployeeDTO(employeeId, "Sahil", "sahil@gmail.com", 26, LocalDate.of(2024,8,26), true);
+    }
+
+    @GetMapping
+    public String getAllEmployees(@RequestParam Integer age) {
+        return "Hi age "+age;
+    }
+
+    @PostMapping
+    public String createNewEmployee() {
+        return "Hello from POST";
+    }
+
+    @PutMapping String updateEmployeeById(){
+        return "Hello from PUT";
+    }
+
 }
